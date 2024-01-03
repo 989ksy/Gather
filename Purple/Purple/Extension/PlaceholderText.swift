ce5cdd094fb251e41d5b3823ccbbd08094e615e9
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-03 21:54:05 +0900

    [Feat] Extension; PlaceholderText

---
+++ Purple/Purple/Extension/PlaceholderText.swift
@@ -0,0 +1,18 @@
+//
+//  Placeholder.swift
+//  Buzz
+//
+//  Created by Seungyeon Kim on 1/3/24.
+//
+
+import UIKit
+
+enum PlaceholderText: String {
+    
+    case email = "이메일을 입력하세요"
+    case nick = "닉네임을 입력하세요"
+    case phoneNum = "전화번호를 입력하세요"
+    case password = "비밀번호를 입력하세요"
+    case checkPassword = "비밀번호를 한 번 더 입력하세요"
+    
+}
