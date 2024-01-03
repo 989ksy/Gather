f929becee996277b2d0374f296ec394f2358d808
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-03 21:53:12 +0900

    [Feat] Componant; ConstantTypoe + Extension

---
+++ Purple/Purple/Constant/ConstantTypo.swift
@@ -0,0 +1,19 @@
+//
+//  ConstantTypo.swift
+//  Buzz
+//
+//  Created by Seungyeon Kim on 1/2/24.
+//
+
+import UIKit
+
+struct ConstantTypo {
+    
+    static let title1 = UIFont.systemFont(ofSize: 22, weight: .bold) // 30
+    static let title2 = UIFont.systemFont(ofSize: 14, weight: .bold) //20
+    static let bodyBold = UIFont.systemFont(ofSize: 13, weight: .bold) //18
+    static let body = UIFont.systemFont(ofSize: 13, weight: .regular) // 18
+    static let caption = UIFont.systemFont(ofSize: 12, weight: .regular) // 12
+    
+    
+}
