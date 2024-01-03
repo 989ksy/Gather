2ec7c45781a8f4be91754eb4da0e79ae57bc7145
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-03 21:52:26 +0900

    [Feat] Componant; TitleLabel + Extension

---
+++ Purple/Purple/Componant/TitleLabel.swift
@@ -0,0 +1,25 @@
+//
+//  TitleLabel.swift
+//  Buzz
+//
+//  Created by Seungyeon Kim on 1/3/24.
+//
+
+import UIKit
+
+class TitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setLabel() {
+        font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        textColor = ConstantColor.blackBrand
+    }
+}
