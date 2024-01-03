b4811dc09d1efffdce6eadd53a62499308792994
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-03 21:51:58 +0900

    [Feat] SignupTextField Extension

---
+++ Purple/Purple/Componant/SignupTextField.swift
@@ -0,0 +1,30 @@
+//
+//  SignupTextField.swift
+//  Buzz
+//
+//  Created by Seungyeon Kim on 1/3/24.
+//
+
+import UIKit
+
+class SignupTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setTextField()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setTextField() {
+        addLeftPadding() // 왼쪽여백 12
+        font = ConstantTypo.body
+        textColor = ConstantColor.blackBrand
+        backgroundColor = ConstantColor.whiteBrand
+        layer.cornerRadius = 8
+        borderStyle = .none
+    }
+    
+}
