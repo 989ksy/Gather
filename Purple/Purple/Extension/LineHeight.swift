3036454f2a2f36f4c47018a6a2f0e4a1f873928e
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-03 21:53:39 +0900

    [Feat] Extension; LineHeight

---
+++ Purple/Purple/Extension/LineHeight.swift
@@ -0,0 +1,24 @@
+//
+//  LineHeight + Extension.swift
+//  Buzz
+//
+//  Created by Seungyeon Kim on 1/2/24.
+//
+
+import UIKit
+
+extension UILabel {
+    func setLineSpacing(spacing: CGFloat) {
+        guard let text = text else { return }
+
+        let attributeString = NSMutableAttributedString(string: text)
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = spacing
+        attributeString.addAttribute(.paragraphStyle,
+                                     value: style,
+                                     range: NSRange(location: 0, length: attributeString.length))
+        attributedText = attributeString
+    }
+    
+}
+
