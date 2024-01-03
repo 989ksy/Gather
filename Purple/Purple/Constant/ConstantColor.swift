d89e98075c54fd1b57c79f34723e7d5fca3e4e36
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-03 21:52:38 +0900

    [Feat] Componant; ConstantColor + Extension

---
+++ Purple/Purple/Constant/ConstantColor.swift
@@ -0,0 +1,36 @@
+//
+//  ConstantColor.swift
+//  Buzz
+//
+//  Created by Seungyeon Kim on 1/2/24.
+//
+
+import UIKit
+
+struct ConstantColor {
+    
+    //MARK: - Brand
+    
+    static let purpleBrand = UIColor(named: "BrandPurple")
+    static let errorBrand = UIColor(named: "BrandError")
+    static let inActiveBrand = UIColor(named: "BrandInactive")
+    static let blackBrand = UIColor(named: "BrandBlack")
+    static let grayBrand = UIColor(named: "BrandGray")
+    static let whiteBrand = UIColor(named: "BrandWhite")
+    
+    //MARK: - Text
+    
+    static let txtPrimary = UIColor(named: "TextPrimary")
+    static let txtSecondary = UIColor(named: "TextSecondary")
+    
+    //MARK: - Background
+    
+    static let bgPrimary = UIColor(named: "BackgroundPrimary")
+    static let bgSecondary = UIColor(named: "BackgroundSecondary")
+    
+    //MARK: - View
+    
+    static let seperator = UIColor(named: "ViewSeperator")
+    static let alpha = UIColor(named: "ViewAlpha")
+    
+}
