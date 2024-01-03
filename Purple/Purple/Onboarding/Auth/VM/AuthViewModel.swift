159312c25c1c028c6b0c93689ac83c6381da681b
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-03 21:55:51 +0900

    [Feat] AuthVM; 회원가입 버튼 rx mvvm

---
+++ Purple/Purple/Onboarding/Auth/VM/AuthViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  AuthViewModel.swift
+//  Buzz
+//
+//  Created by Seungyeon Kim on 1/3/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class AuthViewModel {
+    
+    struct Input {
+        let singupTap : ControlEvent<Void>
+    }
+    
+    struct Output {
+        
+    }
+    
+//    func transform() -> output {
+//        
+//    }
+    
+    
+    
+}
