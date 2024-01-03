98aec5e148947259d9a3bd5be708253cba577b33
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-03 21:53:01 +0900

    [Feat] Componant; ConstantImage + Extension

---
+++ Purple/Purple/Constant/ConstantImage.swift
@@ -0,0 +1,14 @@
+//
+//  ConstantImage.swift
+//  Buzz
+//
+//  Created by Seungyeon Kim on 1/2/24.
+//
+
+import UIKit
+
+struct ConstantImage {
+    
+    static let onboardingImage = UIImage(named: "OnboardingImage")
+    
+}
