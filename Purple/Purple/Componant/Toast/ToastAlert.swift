189cf3d7412ab8244f6ce4b6bf7b53613ac8639c
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-10 20:09:03 +0900

    [Rename] Componant 파일 정리

---
+++ Purple/Purple/Componant/Toast/ToastAlert.swift
@@ -0,0 +1,24 @@
+//
+//  ToastMessage.swift
+//  Purple
+//
+//  Created by Seungyeon Kim on 1/10/24.
+//
+
+import UIKit
+import Toast
+
+extension UIViewController {
+        
+    func setToastAlert(message: String) {
+        DispatchQueue.main.async {
+            var style = ToastStyle()
+            style.backgroundColor = ConstantColor.purpleBrand!
+            style.messageColor = ConstantColor.whiteBrand!
+            style.messageFont = ConstantTypo.body
+            style.messageAlignment = .center
+            self.view.makeToast(message, duration: 3.0, position: .center, style: style)
+        }
+    }
+    
+}
