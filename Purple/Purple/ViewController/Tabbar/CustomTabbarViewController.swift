de10c45c9f2a2da7398fede32110262ffa2b6084
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-01-17 20:20:57 +0900

    [Feat] tabbarController 생성 및 기능

---
+++ Purple/Purple/ViewController/Tabbar/CustomTabbarViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CustomTabbarViewController.swift
+//  Purple
+//
+//  Created by Seungyeon Kim on 1/17/24.
+//
+
+import UIKit
+
+class CustomTabbarViewController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //탭바 배경색
+        self.tabBar.backgroundColor = .brandWhite
+        
+        //탭바 설정
+        //홈
+        let homeTab = HomeInitialViewController()
+        let homeTabItem = UITabBarItem(title: "홈", image: ConstantIcon.homeCustom, tag: 0)
+        homeTab.tabBarItem = homeTabItem
+        homeTab.tabBarItem.selectedImage = ConstantIcon.homeActive?.withRenderingMode(.alwaysOriginal)
+        
+        //DM
+        let dmTab = DMViewController()
+        let dmTabItem = UITabBarItem(title: "DM", image: ConstantIcon.messageCustom, tag: 1)
+        dmTab.tabBarItem = dmTabItem
+        dmTab.tabBarItem.selectedImage = ConstantIcon.messageActive?.withRenderingMode(.alwaysOriginal)
+
+        //검색
+        let searchTab = SearchViewController()
+        let searchTabItem = UITabBarItem(title: "검색", image: ConstantIcon.profile, tag: 2)
+        searchTab.tabBarItem = searchTabItem
+        searchTab.tabBarItem.selectedImage = ConstantIcon.profileActive?.withRenderingMode(.alwaysOriginal)
+        
+        //설정
+        let settingTab = SettingViewController()
+        let settingTabItem = UITabBarItem(title: "설정", image: ConstantIcon.setting, tag: 3)
+        settingTab.tabBarItem = settingTabItem
+        settingTab.tabBarItem.selectedImage = ConstantIcon.settingActive?.withRenderingMode(.alwaysOriginal)
+        
+        //탭바 - 폰트컬러
+        let tabBarAppearance = UITabBarAppearance()
+        let tabBarItemAppearance = UITabBarItemAppearance()
+
+        tabBarItemAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.brandInactive]
+        tabBarItemAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.brandBlack]
+
+        tabBarAppearance.stackedLayoutAppearance = tabBarItemAppearance
+
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+        
+        self.viewControllers = [homeTab, dmTab, searchTab, settingTab]
+    }
+}
