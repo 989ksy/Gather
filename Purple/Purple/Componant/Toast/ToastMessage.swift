96faf965378d053b7cb6e1d3ca556ee00838d295
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-02-08 15:26:28 +0900

    [Edit] 토스트메세지 추가 및 수정

--- Purple/Purple/Componant/Toast/ToastMessage.swift
+++ Purple/Purple/Componant/Toast/ToastMessage.swift
@@ -36,7 +36,7 @@ struct ToastMessage {
     
     enum createChannel: String {
         
-        case nameDuplication = "워크스페이스에 이미 있는 채널 이름입니다. 다른 이름을 입력해주세요."
+        case nameDuplication = "워크스페이스에 이미 있는 채널 이름입니다.\n다른 이름을 입력해주세요."
         case complecated = "채널이 생성되었습니다."
         
     }
